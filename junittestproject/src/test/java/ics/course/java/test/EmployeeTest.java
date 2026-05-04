df75949effcae17f69b089bc17deecf06b7d4aa3
NordstromJoel <jo1102no-s@student.lu.se>
2026-05-04 19:18:19 +0200

    first commit

---
+++ junittestproject/src/test/java/ics/course/java/test/EmployeeTest.java
@@ -0,0 +1,79 @@
+package ics.course.java.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import ics.course.java.Employee;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EmployeeTest {
+    private String expectedName;
+    private String expectedAddress;
+    private String expectedPhone;
+    private Employee e1;
+    private Employee e2;
+
+    @BeforeAll
+    static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterAll
+    static void tearDownAfterClass() throws Exception {
+    }
+
+    @BeforeEach
+    void setUp() throws Exception {
+        expectedName = "Mats";
+        expectedAddress = "Lund";
+        expectedPhone = "12345";
+        e1 = new Employee(expectedName, expectedAddress, expectedPhone);
+        e2 = new Employee("Eva", "Malmö", "54321");
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        e1 = null;
+        e2 = null;
+    }
+
+    @Test
+    final void testGetName() {
+        assertEquals(expectedName, e1.getName());
+    }
+
+    @Test
+    final void testSetName() {
+        String expectedName2 = "Test";
+        e1.setName(expectedName2);
+        assertEquals(expectedName2, e1.getName());
+    }
+
+    @Test
+    final void testGetAddress() {
+        assertNotNull(e1);
+        assertEquals(expectedAddress, e1.getAddress());
+    }
+
+    @Test
+    final void testSetAddress() {
+        String expectedAddress2 = "TestStad";
+        e1.setAddress(expectedAddress2);
+        assertEquals(expectedAddress2, e1.getAddress());
+    }
+
+    @Test
+    final void testGetPhone() {
+        assertEquals(expectedPhone, e1.getPhone());
+    }
+
+    @Test
+    final void testSetPhone() {
+        String expectedPhone2 = "54321";
+        e1.setPhone(expectedPhone2);
+        assertEquals(expectedPhone2, e1.getPhone());
+    }
+}
